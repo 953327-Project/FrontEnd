f9dd63c972b15006dddc18be3aefac39dbff81d1
biggib002 <supachok_ji@cmu.ac.th>
2023-10-18 20:45:13 +0700

    0.1

--- Metric_FrontEnd/src/App.svelte
+++ Metric_FrontEnd/src/App.svelte
@@ -1,47 +1,137 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+	import { onMount } from "svelte";
+  
+	const apiURL = "http://127.0.0.1:5000/get_score_csv";
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+	let proejcts = [];
+	let selected;
+	  
+	onMount(async function() {
+		const response = await fetch(apiURL);
+		proejcts = await response.json();
+
+		// sort array project by score.
+		proejcts.sort(function(a, b){
+			return b.total_score - a.total_score;
+		})
+	});
+  
+	  
+	function deleteProject(index){
+		let tempIndex = 0;
+
+		if(index == null){
+			proejcts.slice(tempIndex, 1);
+		}else{
+			proejcts.splice(index, 1);
+		}
+
+		proejcts = proejcts;
+
+		//console.log(proejcts, index)
+	}
+
+	function handleSubmit() {
+		let index = selected;
+	
+		deleteProject(index);
+	}
+	
+  </script>
+
+<!-- HTML -->
+  
+	<div class="container-fluid">
+		<h2 class="display-4 mb-4">API builder Ranking</h2>	
+
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>🏆#</th>
+					<th>Project Name</th>
+					<th>Total score</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#if proejcts.length}
+						{#each proejcts as project, index}	
+							<tr>
+								<td>{index+1}</td>
+								<td>{project.project_name}</td>
+								<td>{project.total_score}</td>
+							</tr>
+						{/each}
+				{:else}
+					<tr>
+						<td colspan="5">There are no other project</td>
+					</tr>
+				{/if}
+			</tbody>
+		</table>
+
+	{#if proejcts.length}
+		<form on:submit|preventDefault={handleSubmit}>
+			<select bind:value={selected}>
+				{#each proejcts as project, index}
+					<option value={index}>
+						{project.project_name}
+					</option>
+				{/each}
+				<option value="x">
+				</option>
+			</select>
+
+			<button disabled={selected == "x"} type="submit"> Remove </button>
+		</form>
+	{:else}
+		<button on:click={ async () => {
+			const response = await fetch(apiURL);
+			proejcts = await response.json();
+
+			// sort array project by score.
+			proejcts.sort(function(a, b){
+				return b.total_score - a.total_score;
+			})
+		}}>Refresh</button>
+	{/if}
+
+	<hr>
+
+	<p style="opacity: 50%;">Total score calculated by each category, The highest score in the category got 4 point to lowest got 1 point.</p>
+  	
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>Project Name</th>
+				<th>Code Comprehen</th>
+				<th>Doc Comprehen</th>
+				<th>Code Reusability</th>
+				<th>Built Reusability</th>
+				<th>Test Quality</th>
+				<th>Team Activeness</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#if proejcts.length}
+					{#each proejcts as project}	
+						<tr>
+							<td>{project.project_name}</td>
+							<td>{project.code_comprehen}</td>
+							<td>{project.doc_comprehen}</td>
+							<td>{project.code_reusability}</td>
+							<td>{project.built_reusability}</td>
+							<td>{project.test_quality}</td>
+							<td>{project.team_activeness}</td>
+						</tr>
+					{/each}
+			{:else}
+				<tr>
+					<td colspan="7">There are no other project</td>
+				</tr>
+			{/if}
+		</tbody>
+	</table>
+
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
